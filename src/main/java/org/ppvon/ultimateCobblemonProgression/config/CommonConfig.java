8ca0de1324a0428afa793e857ae4e647a6a856a9
TrueDru <ghostpantsdru@gmail.com>
2026-01-17 13:54:13 -0500

    Add options to disable seen/caught requirements

--- src/main/java/org/ppvon/ultimateCobblemonProgression/config/CommonConfig.java
+++ src/main/java/org/ppvon/ultimateCobblemonProgression/config/CommonConfig.java
@@ -3,4 +3,6 @@ package org.ppvon.ultimateCobblemonProgression.config;
 public class CommonConfig {
     public boolean doLevelCap = true;
     public boolean doDexProgression = true;
+    public boolean requireDexSeen = true;
+    public boolean requireDexCaught = true;
 }
