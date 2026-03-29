15cb015ad11fb9ccf46a27bc531daec3aa4fb9a8
Gitoido <warzgibz@gmail.com>
2026-03-29 12:10:19 +0300

    refactor: moved trainer tier widget dimension calculation outside of render func

--- common/src/main/java/org/ppvon/ucp/common/internal/cobblemon/gui/pokedex/TrainerTierInfoWidget.java
+++ common/src/main/java/org/ppvon/ucp/common/internal/cobblemon/gui/pokedex/TrainerTierInfoWidget.java
@@ -1,51 +1,50 @@
 package org.ppvon.ucp.common.internal.cobblemon.gui.pokedex;
 
+import com.cobblemon.mod.common.client.CobblemonResources;
 import com.cobblemon.mod.common.client.gui.summary.widgets.SoundlessWidget;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.player.Player;
 import org.jetbrains.annotations.NotNull;
+import org.ppvon.ucp.common.internal.trainer.TrainerLevelProgression;
 
 import static com.cobblemon.mod.common.api.gui.GuiUtilsKt.blitk;
+import static com.cobblemon.mod.common.client.render.RenderHelperKt.drawScaledText;
 import static org.ppvon.ucp.common.UltimateCobblemonProgression.modId;
 
 public class TrainerTierInfoWidget extends SoundlessWidget {
     private record WidgetData() {
-        public static Integer width = 300;
-        public static Integer height = 40;
+        public static Integer width = 171;
+        public static Integer height = 20;
         public static ResourceLocation bg = modId("textures/gui/pokedex_trainer_tier.png");
     }
 
+    private int screenMiddleX;
+    private int screenMiddleY;
+    private int widgetLeftX;
+    private int widgetTopY;
+    private TrainerLevelProgression.DexCounts dexCounts;
+
     public TrainerTierInfoWidget(int pX, int pY, @NotNull Component component) {
         super(pX, pY, WidgetData.width, WidgetData.height, component);
+        this.setScreenMiddleX(Minecraft.getInstance().getWindow().getGuiScaledWidth() / 2);
+        this.setScreenMiddleY(Minecraft.getInstance().getWindow().getGuiScaledHeight() / 2);
+        this.setWidgetLeftX(this.screenMiddleX - (WidgetData.width / 2));
+        this.setWidgetTopY(this.screenMiddleY - (WidgetData.height / 2));
     }
 
     @Override
     protected void renderWidget(GuiGraphics context, int mouseX, int mouseY, float delta) {
         PoseStack matrices = context.pose();
-        Player player = Minecraft.getInstance().player;
-        player.
-        // base dimensions
-        int baseWidth = 171;
-        int baseHeight = 20;
-        // center of screen
-        int middleX = Minecraft.getInstance().getWindow().getGuiScaledWidth() / 2;
-        int middleY = Minecraft.getInstance().getWindow().getGuiScaledHeight() / 2;
-        // left x and top y of widget relative to the center of screen
-        int leftX = middleX - (baseWidth / 2);
-        int topY = middleY - (baseHeight / 2);
-
-        int x = width / 2;
-        int y = height / 2;
 
         blitk(
                 matrices,
                 WidgetData.bg,
-                leftX + 86,
-                topY - 103,
+                this.widgetLeftX + 86,
+                this.widgetTopY - 102,
                 20,
                 171,
                 0,
@@ -53,5 +52,41 @@ public class TrainerTierInfoWidget extends SoundlessWidget {
                 171,
                 20
         );
+
+        drawScaledText(
+                context,
+                CobblemonResources.INSTANCE.getDEFAULT_LARGE(),
+                Component.translatable("Tier: 1/7").withStyle(Style.EMPTY.withBold(true)),
+                this.widgetLeftX + 128,
+                this.widgetTopY - 97,
+                1F,
+                1F,
+                Integer.MAX_VALUE,
+                0x00FFFFFF,
+                false,
+                true,
+                null,
+                null
+        );
+    }
+
+    public void setScreenMiddleX(int screenMiddleX) {
+        this.screenMiddleX = screenMiddleX;
+    }
+
+    public void setScreenMiddleY(int screenMiddleY) {
+        this.screenMiddleY = screenMiddleY;
+    }
+
+    public void setWidgetLeftX(int widgetLeftX) {
+        this.widgetLeftX = widgetLeftX;
+    }
+
+    public void setWidgetTopY(int widgetTopY) {
+        this.widgetTopY = widgetTopY;
+    }
+
+    public void setDexCounts(TrainerLevelProgression.DexCounts dexCounts) {
+        this.dexCounts = dexCounts;
     }
 }
